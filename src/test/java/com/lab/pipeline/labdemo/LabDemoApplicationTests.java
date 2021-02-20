de91692031546b846ac96b874782291d76c9293d
CompSci2013 <kcollins2004@gmail.com>
2021-02-20 21:51:45 +0000

    Add logic and test

---
+++ src/test/java/com/lab/pipeline/labdemo/LabDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.pipeline.labdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
